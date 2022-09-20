f8fd555efcb368db993b6246c86c58808cbb55c2
Anagha Khanolkar <akhanolkar@google.com>
2022-09-20 18:21:46 -0500

    Added streaming consumer with joins

--- 02-scripts/pyspark/streaming_consumer_with_joins.py
+++ 02-scripts/pyspark/streaming_consumer_with_joins.py
@@ -9,12 +9,13 @@ import sys
 kafkaBrokerAndPortCSV=sys.argv[1]
 kafkaAPIKey=sys.argv[2]
 kafkaAPISecret=sys.argv[3]
-projectID=sys.argv[4]
-bqScratchBucket=sys.argv[5]
-checkpointGCSUri=sys.argv[6]
-bqPromotionsTableFQN=sys.argv[7]
-bqWinnersTableFQN=sys.argv[8]
-printArguments=sys.argv[9]
+kafkaTopicNm=sys.argv[4]
+projectID=sys.argv[5]
+bqScratchBucket=sys.argv[6]
+checkpointGCSUri=sys.argv[7]
+bqPromotionsTableFQN=sys.argv[8]
+bqWinnersTableFQN=sys.argv[9]
+printArguments=sys.argv[10]
 
 # Variables
 kafkaJaasConfig="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"" + kafkaAPIKey + "\" password=\"" + kafkaAPISecret + "\";"
@@ -25,35 +26,35 @@ if printArguments:
     print(f"kafkaBrokerAndPortCSV={kafkaBrokerAndPortCSV}")
     print(f"kafkaAPIKey={kafkaAPIKey}")
     print(f"kafkaAPISecret={kafkaAPISecret}")
+    print(f"kafkaTopicNm={kafkaTopicNm}")
     print(f"projectID={projectID}")
     print(f"bqScratchBucket={bqScratchBucket}")
     print(f"checkpointGCSUri={checkpointGCSUri}")
     print(f"bqPromotionsTableFQN={bqPromotionsTableFQN}")
+    print(f"bqWinnersTableFQN={bqWinnersTableFQN}")
     print("kafka.sasl.jaas.config="+kafkaJaasConfig)
 #}}
 
 # Spark session
 spark = SparkSession \
         .builder \
-        .appName("entries-consumer") \
+        .appName("winners-consumer") \
         .config("spark.streaming.stopGracefullyOnShutdown", "true") \
         .config("spark.sql.streaming.schemaInference", "true") \
         .getOrCreate()
-
-# Variables
-kafkaTopic="entries"
 
 # Config for use by Spark BQ connector
 spark.conf.set("parentProject", projectID)
 spark.conf.set("temporaryGcsBucket",bqScratchBucket)
 
 # Read static source into a dataframe - Promotions data in BigQuery
-promotionsDF = spark.read.format("bigquery").option("table",bqPromotionsTableFQN).load()
+promotionsStaticDF = spark.read.format("bigquery").option("table",bqPromotionsTableFQN).load()
+promotionsStaticDF.show(2)
 
 # Read from Kafka topic
-entriesRawDF = spark.readStream.format("kafka") \
+entriesRawStreamingDF = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", kafkaBrokerAndPortCSV) \
-    .option("subscribe", kafkaTopic) \
+    .option("subscribe", kafkaTopicNm) \
     .option("kafka.security.protocol", "SASL_SSL") \
     .option("kafka.sasl.mechanism", "PLAIN") \
     .option("kafka.sasl.jaas.config", kafkaJaasConfig) \
@@ -63,7 +64,7 @@ entriesRawDF = spark.readStream.format("kafka") \
 
 
 # Select key and payload and cast as string
-entriesKvDF = entriesRawDF.selectExpr("CAST(key AS STRING) as case_id", "CAST(value AS STRING) as json_payload")
+entriesKvStreamingDF = entriesRawStreamingDF.selectExpr("CAST(key AS STRING) as case_id", "CAST(value AS STRING) as json_payload")
 
 # Define schema for parsing payload
 schema = StructType([ 
@@ -75,13 +76,15 @@ schema = StructType([
   ])
 
 # Parse the JSON payload into individual columns
-entriesParsedDF = entriesKvDF.withColumn("jsonData",from_json(col("json_payload"),schema)).select("jsonData.*")
+entriesParsedStreamingDF = entriesKvStreamingDF.withColumn("jsonData",from_json(col("json_payload"),schema)).select("jsonData.*")
 
 # Rename columns 
-entriesDF=entriesParsedDF.toDF("email","name","entry_time","day","participation_number")
+entriesStreamingDF=entriesParsedStreamingDF.toDF("email","name","entry_time","day","participation_number")
+entriesStreamingDF.show(2)
 
 # Inner join the stream with the static data to determine winners
-joinedDF=entriesDF.join(promotionsDF, (entriesDF.day == promotionsDF.day) & (entriesDF.participation_number == promotionsDF.participation_number),"inner").drop(promotionsDF.participation_number).drop(promotionsDF.day)  
+joinedDF=entriesStreamingDF.join(promotionsStaticDF, (entriesStreamingDF.day == promotionsStaticDF.day) & (entriesStreamingDF.participation_number == promotionsStaticDF.participation_number),"inner").drop(promotionsStaticDF.participation_number).drop(promotionsStaticDF.day)  
+joinedDF.show(2)
 
 # Process the stream joined with static data into a precreated BigQuery table in append mode
 queryDF=joinedDF.writeStream.format("bigquery").outputMode("append").option("table", bqWinnersTableFQN).option("checkpointLocation", checkpointGCSUri).start()
